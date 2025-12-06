388de5a58e335068cd3c17e81a3f3bd0eff42ced
Zayyan Abdillah <muhammad.abdillah@ist.id>
2025-12-06 15:41:58 +0700

    merge sort algorithm

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,61 @@
-public static void main(String[] args) {
+import java.util.Arrays;
 
+public static void main(String[] args) {
+    int data[] = {45, 12, 90, 32, 67, 81, 10, 55};
+    System.out.println("Before sorting: " + Arrays.toString(data));
+
+    mergeSort(data, 0, data.length - 1);
+
+    System.out.println("After sorting descending: " + Arrays.toString(data));
 }
 
+private static void mergeSort(int arr[], int left, int right) {
+    if (left < right) {
+        int mid = (left + right) / 2;
+
+        mergeSort(arr, left, mid);
+        mergeSort(arr, mid + 1, right);
+
+        merge(arr, left, mid, right);
+    }
+}
+
+private static void merge(int arr[], int left, int mid, int right) {
+
+    int n1 = mid - left + 1;
+    int n2 = right - mid;
+
+    int L[] = new int[n1];
+    int R[] = new int[n2];
+
+    for (int i = 0; i < n1; i++)
+        L[i] = arr[left + i];
+    for (int j = 0; j < n2; j++)
+        R[j] = arr[mid + 1 + j];
+
+    int i = 0, j = 0;
+    int k = left;
+
+    while (i < n1 && j < n2) {
+        if (L[i] >= R[j]) {
+            arr[k] = L[i];
+            i++;
+        } else {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+
+    // Copy sisa elemen (jika ada)
+    while (i < n1) {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+    while (j < n2) {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
+}
