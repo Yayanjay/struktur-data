e2adfbf6fe7d77c87578fbc4074874b4da7688f2
Zayyan Abdillah <muhammad.abdillah@ist.id>
2025-12-07 19:24:21 +0700

    dfs

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,35 +1,39 @@
-import java.util.Arrays;
-import java.util.LinkedList;
+
+static int[][] graph = {
+        {1, 3},   // a1 -> a2, a4
+        {2, 4},   // a2 -> a3, a5
+        {5},      // a3 -> a6
+        {},       // a4 (leaf)
+        {6},      // a5 -> a7
+        {},       // a6 (leaf)
+        {7},      // a7 -> a8
+        {}        // a8 (leaf)
+};
+
+static boolean[] visited = new boolean[8];
 
 public static void main(String[] args) {
-    // Buatlah sebuah deklarasi variabel dengan tipe data integer yang bernama ‘StrukturBaris’
-    int StrukturBaris;
+    int start = 0; // mulai dari a1 (index 0)
+    int target = 7; // cari a8 (index 7)
 
-    // Buatlah sebuah deklarasi variabel dengan tipe data string yang bernama ‘KataBaru’ yang berisi kata ‘Deklarasi tipe data String’
-    String KataBaru = "Deklarasi tipe data String";
-    System.out.println(KataBaru);
-
-    // Buatlah pendeklarasian array satu dimensi dengan nama ‘empatAngka’, tipe data integer, yang berisi angka (07, 10, 20, 23) Pendeklarasian tersebut dengan menggunakan bahasa pemrograman Java.
-    int[] empatAngka = {07, 10, 20, 23};
-    System.out.println(Arrays.toString(empatAngka));
-
-    // Buatlah pendeklarasian array dua dimensi dengan nama ‘Angka’, tipe data String, yang terdiri dari tiga baris dan tiga kolom, isi baris dan kolom berisi angka berikut (1, 3, 5, 14, 19, 20, 22, 27, 29) Pendeklarasian tersebut dengan menggunakan bahasa pemrograman Java.
-    String[][] Angka = {
-            {"1", "3", "5"},
-            {"14", "19", "20"},
-            {"22", "27", "29"}
-    };
-    System.out.println(Arrays.deepToString(Angka));
-
-    // Buatlah deklarasi linked list dengan nama 'listAngka' yang memiliki list (22, 19, 44, 60, 72), Pendeklarasian tersebut dengan menggunakan bahasa pemrograman Java.
-    LinkedList<Integer> listAngka = new LinkedList<Integer>();
-    listAngka.add(22);
-    listAngka.add(19);
-    listAngka.add(44);
-    listAngka.add(60);
-    listAngka.add(72);
-
-    System.out.println(listAngka);
-
+    System.out.println("DFS dari a1, nyari a8:");
+    dfs(start, target);
 }
 
+static void dfs(int node, int target) {
+    visited[node] = true;
+
+    String namaNode = "a" + (node + 1);
+    System.out.println("visited " + namaNode);
+
+    if (node == target) {
+        System.out.println("found target " + namaNode);
+        return;
+    }
+
+    for (int tetangga : graph[node]) {
+        if (!visited[tetangga]) {
+            dfs(tetangga, target);
+        }
+    }
+}
